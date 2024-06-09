2f0e8b7b070ba322af13ee3faabae30629b42fcd
JASPER <jasjasper101@gmail.com>
2024-06-08 23:00:22 -0700

    Remove helper functions

--- components/ContentList.js
+++ components/ContentList.js
@@ -2,7 +2,6 @@
 import { useState, useRef, useEffect } from 'react';
 import Image from 'next/image';
 import Content from './Content';
-import { handleDragOver, handleDragStart, handleDrop } from './helperFunctions';
 
 export default function ContentList({ items }) {
   const [contentData, setContentData] = useState(items || []);
@@ -18,6 +17,57 @@ export default function ContentList({ items }) {
       img.src = item.image;
     });
   }, [contentData]);
+
+  const handleDragStart = (index, event) => {
+    setDraggedIndex(index);
+    setDraggedItem(contentData[index]);
+
+    const dragImage = document.createElement('div');
+    dragImage.style.width = '288px';
+    dragImage.style.height = '75px';
+    dragImage.style.display = 'flex';
+    dragImage.style.alignItems = 'center';
+    dragImage.style.backgroundColor = 'white';
+    dragImage.style.border = '1.5px solid gray';
+    dragImage.style.padding = '5px';
+    dragImage.style.borderRadius = '4px';
+    dragImage.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
+
+    const img = document.createElement('img');
+    img.src = contentData[index].image;
+    img.width = 45;
+    img.height = 45;
+    img.style.marginRight = '10px';
+    dragImage.appendChild(img);
+
+    const text = document.createElement('p');
+    text.innerText = contentData[index].title;
+    text.style.fontWeight = 'bold';
+    text.style.fontSize = '17px';
+    dragImage.appendChild(text);
+
+    document.body.appendChild(dragImage);
+    dragImageRef.current = dragImage;
+
+    event.dataTransfer.setDragImage(dragImage, 0, 0);
+  };
+
+  const handleDragOver = (index) => {
+    setHoveredIndex(index);
+  };
+
+  const handleDrop = (index) => {
+    if (draggedItem) {
+      const updatedItems = [...contentData];
+      updatedItems.splice(draggedIndex, 1);
+      updatedItems.splice(index, 0, draggedItem);
+
+      setContentData(updatedItems);
+      setDraggedItem(null);
+      setDraggedIndex(null);
+      setHoveredIndex(null);
+    }
+  };
 
   const handleDragEnd = async () => {
     setDraggedItem(null);
